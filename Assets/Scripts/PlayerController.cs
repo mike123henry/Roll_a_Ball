eee96ae967a8c54544a19170dda1da359b02f914
MikeH <mike123.henry@gmail.com>
2017-07-21 14:03:55 -0500

    added script to count collected objects and display win text when a preset number of objects are collected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
 
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -27,6 +34,16 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count = count +1;
+            setCountText();
+        }
+    }
+    void setCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "YOU WIN";
         }
     }
 }
